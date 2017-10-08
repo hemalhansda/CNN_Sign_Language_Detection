9caf1aa3fe65d81e344ada46576d321d5f1a6a82
Karthick <karthick.arya@teachforindia.org>
2017-10-08 18:47:46 +0530

    changed to 5 classes

--- image_transformations.py
+++ image_transformations.py
@@ -27,7 +27,7 @@ def main():
             for x_batch in datagen.flow(x, batch_size=1, shuffle=False):
                 training_data.append([x_batch[0], y])
                 i += 1
-                if i > 15:
+                if i > 20:
                     break
 
         np.save("{}_new_training_data_1.npy".format(label), training_data)
